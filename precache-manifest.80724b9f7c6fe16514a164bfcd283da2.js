fb55fcb7b3652b6c347677e62a8c137f8b851138
Abhishek Gowda <abhi.gowda28@gmail.com>
2020-05-25 20:42:38 +0530

    Updates

---
+++ precache-manifest.80724b9f7c6fe16514a164bfcd283da2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f8fcc32376b7a774945a283ff5469f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "492235ff422512c09f09",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b4ac26398cf384eed192",
+    "url": "/static/js/2.75213c24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75213c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "492235ff422512c09f09",
+    "url": "/static/js/main.f923ee33.chunk.js"
+  },
+  {
+    "revision": "11f1b02b32c27a086430",
+    "url": "/static/js/runtime-main.b24d7b65.js"
+  }
+]);
